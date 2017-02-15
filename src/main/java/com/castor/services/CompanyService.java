288ce41c487621387f4138161c41f61636fa4cdb
Vijay Gajula <vgajula@care.com>
2017-02-15 16:54:44 +0530

    start code

---
+++ src/main/java/com/castor/services/CompanyService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.castor.services;
+
+import com.castor.model.Company;
+
+/**
+ * @author satish
+ *
+ */
+public interface CompanyService {
+	/**
+	 * It creates the company.
+	 * 
+	 * @return
+	 */
+	Company establish();
+}
---
+++ src/test/java/com/castor/test/XMLConverterServiceTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.castor.test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author satish
+ *
+ */
+public class XMLConverterServiceTest extends TestCase {
+
+	public void testConvertFromObjectToXML() {
+		// write the unit test code here for test the ConvertFromObjectToXML()
+	}
+	public void testconvertFromXMLToObject() {
+		// write the unit test code here for test the convertFromXMLToObject()
+	}
+}
